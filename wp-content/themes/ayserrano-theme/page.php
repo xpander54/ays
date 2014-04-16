5b3d84f8e0adc34bd59cf82611654cdcd802f975
xpander54 <rodrigo@holbox.in>
2014-04-15 22:16:43 -0500

    index sin construcción

--- wp-content/themes/ayserrano-theme/page.php
+++ wp-content/themes/ayserrano-theme/page.php
@@ -21,312 +21,34 @@
 							        				El <span class='gris'>A</span><span class='rojo'>B</span><span class='azul'>C</span> de los <strong>seguros</strong>
 							      				</div> -->
 
-						<div class="sitio-temporal">
-							
-							<a style="font-family: 'Coda', sans-serif; font-size: 3.3em; " href="http://www.ayserranosc.wix.com/inicio">Visita sitio temporal</a>
 						
-						</div>
 						
-						<img src="<?php bloginfo('template_url');?>/img/construccion.png" alt="en construccion" class="thumbnail-img">
-
+						
 					</div>
-					
+					<br><br>
 					<div class="row">
 
-						<div class="col-md-6 col-sm-6 col-sm-offset-1 col-md-offset-0">
-							<div class="row">
-								
-								<div class="col-md-4 col-sm-4">
-									<h2 class=subtitle>
-										
-										Coberturas
-									</h2>
-
-								</div>
-								<div class="col-md-6 col-sm-6 line-subtitle">
-									
-								</div>
-
-							</div>
-
-							<div class="row">
-								
-
-								<div class="coberturas-index">
-									<ul class="nav nav-tabs tabs-index">
-									  <li class="active">
-									  		
-									  		<a href="#basica" data-toggle="tab">
-									  			Basica
-									  		</a>
-									  	</li>
-									  <li>
-									  		<a href="#juridica" data-toggle="tab">
-									  			Juridica
-									  		</a>
-									  		
-									  	</li>
-									  <li>
-									  	<a href="#jmasd" data-toggle="tab">
-									  		Juridica + Daños
-									  	</a>
-									  </li>
-									</ul>
-								</div>
-
-								<div class="tab-content">
-									
-
-
-									<div class="tab-pane active" id="basica">
-										<div class="cobertura-img">
-								  			 
-								  			 <!-- slide-index2 -->
-												
-											<div class="slide-index2">
-												
-												<div id="carousel-2" class="carousel slide carousel2">
-				 
-				                
-				 
-												                <!-- Carousel items -->
-												                <div class="carousel-inner">
-												 
-												                  
-																	<div class="active item">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI3a.jpg" class="slide-img img-thumbnail" alt="DF">									
-															        </div>
-															        <div class="item">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI1.jpg" class="slide-img img img-thumbnail" alt="bienes raices">
-															        </div>
-															        <div class="item">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI2.jpg" class="slide-img img img-thumbnail" alt="bienes raices">
-															        </div>
-
-
-
-												 
-												                </div>
-
-												                <div class="indicadores">
-												                	
-
-
-													                <!-- <ol class="carousel-indicators">
-													                		
-
-													                  <li data-target="#carousel-2" data-slide-to="0" class="active"></li>
-													                  <li data-target="#carousel-2" data-slide-to="1"></li>
-													                  <li data-target="#carousel-2" data-slide-to="2"></li>
-													                  <li data-target="#carousel-2" data-slide-to="3"></li>
-													                		
-
-													                </ol> -->
-													 
-												                </div>
-												                 
-												                <a style='font-size: 2em !important;' class="carousel-control left" href="#carousel-2" data-slide="prev">&lsaquo;</a>
-												                <a style='font-size: 2em !important;' class="carousel-control right" href="#carousel-2" data-slide="next">&rsaquo;</a>
-												                
-												                
-												                
-												 </div>
-
-											</div>
-
-											<!-- /slide-index2 -->
-
-								  			<br>
-								  			<p>
-								  				Recuperación extrajudicial de las rentas no cubiertas.
-								  			</p>
-								  			<a href="/coberturas" style='font-size: 1em; left: 0;' class='top-btn'>
-								  				ver más
-								  			</a>
-								  			
-								  		</div>
-								  		<!-- /Cobertura img -->
-
-									</div>
-
-									<div class="tab-pane" id="juridica">
-
-
-										<div class="cobertura-img">
-								  			 
-								  			 <!-- slide-index2 -->
-												
-											<div class="slide-index2">
-												
-												<div id="carousel-3" class="carousel slide carousel2">
-				 
-				                
-				 
-												                <!-- Carousel items -->
-												                <div class="carousel-inner">
-												 
-																	<div class="active item">
-												                  <a href="/coberturas">
-												                  	
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI1a.jpg" class="slide-img img-thumbnail" alt="DF">									
-												                  </a>
-															        </div>
-															        <div class="item">
-															          	<a href="/coberturas">
-												                  			
-															          		<img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI3.jpg" class="slide-img img img-thumbnail" alt="bienes raices">
-												                  		
-												                  		</a>
-															        </div>
-															        <div class="item">
-															          
-															          <a href="/coberturas">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI2.jpg" class="slide-img img img-thumbnail" alt="bienes raices">
-															          </a>
-															        </div>
-
-
-
-												 
-												                </div>
-
-												                <div class="indicadores">
-												                	
-
-
-													                <!-- <ol class="carousel-indicators">
-													                					 
-													                  <li data-target="#carousel-3" data-slide-to="0" class="active"></li>
-													                  <li data-target="#carousel-3" data-slide-to="1"></li>
-													                  <li data-target="#carousel-3" data-slide-to="2"></li>
-													                  <li data-target="#carousel-3" data-slide-to="3"></li>
-													                					 
-													                </ol> -->
-													 
-												                </div>
-												                 
-												                <a style='font-size: 2em !important;' class="carousel-control left" href="#carousel-3" data-slide="prev">&lsaquo;</a>
-												                <a style='font-size: 2em !important;' class="carousel-control right" href="#carousel-3" data-slide="next">&rsaquo;</a>
-												                
-												                
-												                
-												 </div>
-
-											</div>
-
-											<!-- /slide-index2 -->
-
-								  			<br>
-								  			<p>
-								  				<a href="/coberturas">
-								  					Resicisión del contrato por incumplimiento de pago
-								  				</a>
-								  			</p>
-
-								  				<a href="/coberturas" style='font-size: 1em; left: 0;' class='top-btn'>
-								  				ver más
-								  			</a>
-								  			
-								  		</div>
-								  		<!-- /Cobertura img -->
-
-										
-
-										
-									</div>
-									<div class="tab-pane" id="jmasd">
-
-										
-										<div class="cobertura-img">
-								  			 
-								  			 <!-- slide-index2 -->
-												
-											<div class="slide-index2">
-												
-												<div id="carousel-4" class="carousel slide carousel2">
-				 
-				                
-				 
-												                <!-- Carousel items -->
-												                <div class="carousel-inner">
-												 
-												                  
-																	<div class="active item">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI2a.jpg" class="slide-img img-thumbnail" alt="DF">									
-															        </div>
-															        <div class="item">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI1.jpg" class="slide-img img img-thumbnail" alt="bienes raices">
-															        </div>
-															        <div class="item">
-															          <img src="<?php bloginfo('template_url');?>/img/coberturas2/sCI3.jpg" class="slide-img img img-thumbnail" alt="bienes raices">
-															        </div>
-
-
-
-												 
-												                </div>
-
-												                <div class="indicadores">
-												                	
-
-
-													                <!-- <ol class="carousel-indicators">
-													                					 
-													                  <li data-target="#carousel-4" data-slide-to="0" class="active"></li>
-													                  <li data-target="#carousel-4" data-slide-to="1"></li>
-													                  <li data-target="#carousel-4" data-slide-to="2"></li>
-													                  <li data-target="#carousel-4" data-slide-to="3"></li>
-													                					 
-													                </ol> -->
-													 
-												                </div>
-												                 
-												                <a style='font-size: 2em !important;' class="carousel-control left" href="#carousel-4" data-slide="prev">&lsaquo;</a>
-												                <a style='font-size: 2em !important;' class="carousel-control right" href="#carousel-4" data-slide="next">&rsaquo;</a>
-												                
-												                
-												                
-												 </div>
-
-											</div>
-
-											<!-- /slide-index2 -->
-
-								  			<br>
-								  			<p>
-								  				Recuperación de pago de servicios y de daños al inmueble.
-								  			</p>
-								  			<a href="/coberturas" style='font-size: 1em; left: 0;' class='top-btn'>
-								  				ver más
-								  			</a>
-								  		</div>
-								  		<!-- /Cobertura img -->
-
-										
-									</div>
-							
-								</div>
-
-							</div>
-
-
-						</div> 
+											
 						
 						<!-- termina col left -->
 
-						<div class="col-md-5 col-sm-5">
+						<div class=" col-sm-12 ">
 
 							 <div class="row">
+
+							 	<div class=" col-sm-2 line-subtitle">
+						
+								</div>	
 								
 								
-								<div class="col-md-4 col-sm-4 col-subtitle">
-										<h2 class=subtitle>
+								<div class="col-sm-2 col-subtitle">
+										<h2 class='subtitle'>
 											
 											Empresa
 										</h2>
 
 								</div>
-								<div class="col-md-6 col-sm-6 line-subtitle">
+								<div class="col-sm-7 line-subtitle">
 									
 								</div>
 
@@ -393,7 +115,7 @@
 								        </a>
 								      </h4>
 								    </div>
-								    <div id="collapseTwo" class="panel-collapse collapse">
+								    <div id="collapseTwo" class="panel-collapse collapse in">
 								      <div class="panel-body">
 
 								      	<ul>
